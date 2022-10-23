2c6104eb57999bf3d594f855ee12eae234410d4d
noeppi_noeppi <noeppinoeppi@gmail.com>
2022-10-23 18:24:47 +0200

    Put task logic into Task.TaskContent

--- src/main/java/io/github/noeppi_noeppi/mods/bongo/task/Task.java
+++ src/main/java/io/github/noeppi_noeppi/mods/bongo/task/Task.java
@@ -23,107 +23,122 @@ import java.util.stream.Stream;
 
 public class Task {
 
-    public static Task EMPTY = new Task(TaskTypeEmpty.INSTANCE, Unit.INSTANCE);
+    public static Task EMPTY = new Task(new TaskContent(TaskTypeEmpty.INSTANCE, Unit.INSTANCE));
 
-    @SuppressWarnings("unchecked")
-    public static final Codec<Task> CODEC = MoreCodecs.mapDispatch(
-            TaskTypes.CODEC.fieldOf("type"),
-            TaskTypes::getCodec,
-            task -> Pair.of(task.type, task.element),
-            (type, element) -> CodecHelper.doesNotThrow(() -> new Task((TaskType<Object>) type, element))
-    );
-
-    private final TaskType<?> type;
-    private final Object element;
-
+    public static final Codec<Task> CODEC = TaskContent.CODEC.xmap(Task::new, t -> t.content);
+    
+    private final TaskContent content;
+    
     public <T> Task(TaskType<T> type, T element) {
-        this.type = type;
-        this.element = element;
-        validateElementType();
+        this(new TaskContent(type, element));
+    }
+    
+    private Task(TaskContent content) {
+        this.content = content;
     }
 
     public TaskType<?> getType() {
-        return type;
+        return content.type;
     }
 
     public Component typeName() {
-        return this.type.name();
+        return this.content.type.name();
     }
     
     public Component contentName(@Nullable MinecraftServer server) {
         //noinspection unchecked
-        return ((TaskType<Object>) this.type).contentName(this.element, server);
+        return ((TaskType<Object>) this.content.type).contentName(this.content.element, server);
     }
     
     public void sync(MinecraftServer server, @Nullable ServerPlayer target) {
         //noinspection unchecked
-        ((TaskType<Object>) this.type).sync(this.element, server, target);
+        ((TaskType<Object>) this.content.type).sync(this.content.element, server, target);
     }
     
     public <T> boolean shouldComplete(TaskType<T> type, ServerPlayer player, T compare) {
-        if (this.type == type) {
+        if (this.content.type == type) {
             //noinspection unchecked
-            return ((TaskType<Object>) this.type).shouldComplete(player, this.element, compare);
+            return ((TaskType<Object>) this.content.type).shouldComplete(player, this.content.element, compare);
         } else {
             return false;
         }
     }
     
     public <T> void consume(TaskType<T> type, ServerPlayer player, T found) {
-        if (this.type == type) {
+        if (this.content.type == type) {
             //noinspection unchecked
-            ((TaskType<Object>) this.type).consume(player, this.element, found);
+            ((TaskType<Object>) this.content.type).consume(player, this.content.element, found);
         }
     }
     
     public Stream<Highlight<?>> highlight() {
         //noinspection unchecked
-        return ((TaskType<Object>) this.type).highlight(this.element);
+        return ((TaskType<Object>) this.content.type).highlight(this.content.element);
     }
     
     public void invalidate() {
         //noinspection unchecked
-        ((TaskType<Object>) this.type).invalidate(this.element);
+        ((TaskType<Object>) this.content.type).invalidate(this.content.element);
     }
 
     @OnlyIn(Dist.CLIENT)
     public FormattedCharSequence renderDisplayName(Minecraft mc) {
         //noinspection unchecked
-        return ((TaskType<Object>) this.type).renderDisplayName(mc, this.element);
+        return ((TaskType<Object>) this.content.type).renderDisplayName(mc, this.content.element);
     }
     
     @OnlyIn(Dist.CLIENT)
     public void renderSlot(Minecraft mc, PoseStack poseStack, MultiBufferSource buffer) {
-        this.type.renderSlot(mc, poseStack, buffer);
+        this.content.type.renderSlot(mc, poseStack, buffer);
     }
     
     @OnlyIn(Dist.CLIENT)
     public void renderSlotContent(Minecraft mc, PoseStack poseStack, MultiBufferSource buffer, boolean bigBongo) {
         //noinspection unchecked
-        ((TaskType<Object>) this.type).renderSlotContent(mc, this.element, poseStack, buffer, bigBongo);
+        ((TaskType<Object>) this.content.type).renderSlotContent(mc, this.content.element, poseStack, buffer, bigBongo);
     }
     
     public void validate(MinecraftServer server) {
         try {
             //noinspection unchecked
-            ((TaskType<Object>) type).validate(element, server);
+            ((TaskType<Object>) this.content.type).validate(this.content.element, server);
         } catch (Exception e) {
-            BongoMod.logger.error("Failed to validate task of type {}: {}", type.id(), e.getMessage());
+            BongoMod.logger.error("Failed to validate task of type {}: {}", this.content.type.id(), e.getMessage());
         }
     }
     
     public <T> Optional<T> getElement(TaskType<T> type) {
-        if (this.type == type) {
+        if (this.content.type == type) {
             //noinspection unchecked
-            return Optional.of((T) element);
+            return Optional.of((T) this.content.element);
         } else {
             return Optional.empty();
         }
     }
     
-    private void validateElementType() {
-        if (!type.taskClass().isAssignableFrom(element.getClass())) {
-            throw new IllegalStateException("Can't create task of type " + type.id() + " with element of type " + element.getClass());
+    private static class TaskContent {
+
+        @SuppressWarnings("unchecked")
+        public static final Codec<TaskContent> CODEC = MoreCodecs.mapDispatch(
+                TaskTypes.CODEC.fieldOf("type"),
+                TaskTypes::getCodec,
+                content -> Pair.of(content.type, content.element),
+                (type, element) -> CodecHelper.doesNotThrow(() -> new TaskContent((TaskType<Object>) type, element))
+        );
+        
+        private final TaskType<?> type;
+        private final Object element;
+
+        private <T> TaskContent(TaskType<T> type, T element) {
+            this.type = type;
+            this.element = element;
+            validateElementType();
+        }
+    
+        private void validateElementType() {
+            if (!type.taskClass().isAssignableFrom(element.getClass())) {
+                throw new IllegalStateException("Can't create task of type " + type.id() + " with element of type " + element.getClass());
+            }
         }
     }
 }
